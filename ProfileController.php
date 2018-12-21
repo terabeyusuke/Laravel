694feaa751efacf18a18167c19dc638db756a570
寺部　裕介 <yusuketerabe@terabuyuusukenoMacBook-Air.local>
2018-12-21 17:29:08 +0900

    frst commit

---
+++ ProfileController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ProfileController extends Controller
+
+{
+    //
+}
+
+
+public function edit()
+{
+  return view('admin.profile.edit');
+}
+public function update()
+{
+  return redirect('adin/profile/edit');
+}
